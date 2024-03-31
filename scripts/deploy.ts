b600704568acc07bab344a65b8fdd67a51b3c137
omourissoux <73721926+omourissoux@users.noreply.github.com>
2024-03-31 19:48:14 +0200

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -18,15 +18,3 @@ main()
     process.exit(1);
   });
 
-//   const contractAddress = 'ADRESSE_DU_CONTRAT'; // Remplacez par l'adresse de votre contrat
-// const provider = new ethers.providers.JsonRpcProvider('URL_DU_RPC_POLYGON'); // URL du provider RPC de Polygon
-// const signer = new ethers.Wallet('VOTRE_CLÉ_PRIVÉE', provider); // Remplacez par votre clé privée
-// const contract = new ethers.Contract(contractAddress, ABI_DU_CONTRAT, signer); // Remplacez ABI_DU_CONTRAT par l'ABI de votre contrat
-
-// async function addToWhitelist(address) {
-//     const tx = await contract.addToWhitelist(address);
-//     await tx.wait();
-//     console.log(`Adresse ${address} ajoutée à la whitelist`);
-// }
-
-// addToWhitelist(wallet.address); // Utilisez l'adresse générée précédemment
